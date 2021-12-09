2e260a0327108853c5d02d21f0123617b69ab78b
Rupesh Kumar <rupeshkmar116@gmail.com>
2021-12-09 19:23:40 +0530

    pull-new4

--- router/hello.py
+++ router/hello.py
@@ -1,23 +1,7 @@
-import time
-
-import redis
-from flask import Flask
-
-app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
-
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
-
-@app.route('/')
-def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+print('hello world, i am rupesh')
+a = 1
+b = 2
+c = a + b
+print ("total", +c)
+print ('thank you')
+print ('i am good in devops')
